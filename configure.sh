67057e98115028c23b87c90335d2515b8436ab65
kalagxw <kalagxw@gmail.com>
2020-10-01 17:50:30 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# V2Ray new configuration
-echo "$CONFIG_JSON" > /etc/v2ray/config.json
-# Run V2Ray
-/usr/bin/v2ray/v2ray -config=/etc/v2ray/config.json
+# fd929 new configuration
+echo "$CONFIG_JSON" > /etc/fd929/config.json
+# Run fd929
+/usr/bin/fd929/fd929 -config=/etc/fd929/config.json
